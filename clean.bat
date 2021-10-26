5c7bc7091140b0a54076f91368c89d8999e1346b
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2021-10-26 09:28:56 +0900

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,15 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-del artifacts\Debug\*.exe
-del artifacts\Debug\*.exe32
-del artifacts\Debug\*.dll
-del artifacts\Debug\*.dll32
-
-del artifacts\Release\*.exe
-del artifacts\Release\*.exe32
-del artifacts\Release\*.dll
-del artifacts\Release\*.dll32
+rmdir /s /q artifacts
 
 rmdir /s /q vs2019\dll\.vs
 del vs2019\dll\*.vcxproj.user
